882e40eb3c99b984e04ba79fe44dc2c533ed7efe
RonaldoFM <43863628+RonaldoFM@users.noreply.github.com>
2026-01-27 22:28:25 -0300

    Add project files.

---
+++ ProducerConsumerRabbitMQ/Consumer/Receiver.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+await using var connection = await factory.CreateConnectionAsync();
+await using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(
+    queue: "BasicTest",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+);
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+
+consumer.ReceivedAsync += async (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {message}");
+    await Task.Yield();
+};
+
+await channel.BasicConsumeAsync("BasicTest", true, consumer);
+
+
+Console.WriteLine("Press [Enter] to exit the Consumer app..");
+Console.ReadLine();
---
+++ ProducerConsumerRabbitMQ/Producer/Sender.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+await using var connection = await factory.CreateConnectionAsync();
+await using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(
+    queue: "BasicTest",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+);
+
+string message = "Getting started with .Net Core RabbitMQ!";
+var body = Encoding.UTF8.GetBytes(message);
+
+await channel.BasicPublishAsync(
+    exchange: "",
+    routingKey: "BasicTest",
+    body: body
+);
+
+Console.WriteLine($" [x] Sent {message}");
+
+Console.WriteLine("Press [Enter] to exit the sender app..");
+Console.ReadLine();
